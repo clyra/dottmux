6a69c4d5baeb5301e96036cd2f42f3373b6a79c3
Christian Lyra <poplyra@gmail.com>
2023-05-07 11:44:43 -0300

    few improvements to the bootstrap script

--- bootstrap_tmux.sh
+++ bootstrap_tmux.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 cd
-git clone https://github.com/gpakosz/.tmux.git ".config/oh-my-tmux"
-ln -s .config/oh-my-tmux/.tmux.conf .tmux.conf
 
-git clone https://github.com/clyra/dottmux.git ".config/oh-my-tmux-local"
-ln -s .config/oh-my-tmux-local/.tmux.conf.local .tmux.conf.local
+if [ -d ".config/oh-my-tmux" ] 
+then
+	echo "updating oh-my-tmux"
+	cd .config/oh-my-tmux
+        git pull
+else
+       echo "cloning oh-my-tmux"
+       git clone https://github.com/gpakosz/.tmux.git ".config/oh-my-tmux"
+       ln -s .config/oh-my-tmux/.tmux.conf .tmux.conf
+fi
 
+cd
+if [ -d ".config/oh-my-tmux-local" ] 
+then
+	echo "updating oh-my-tmux-local"
+	cd .config/oh-my-tmux-local
+        git pull
+else
+       echo "cloning oh-my-tmux-local"
+	git clone https://github.com/clyra/dottmux.git ".config/oh-my-tmux-local"
+	ln -s .config/oh-my-tmux-local/.tmux.conf.local .tmux.conf.local
+fi
 
+cd

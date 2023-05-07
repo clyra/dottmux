8421ed74539a25348c48e4d7ac3b2d0153e9bcad
Christian Lyra <poplyra@gmail.com>
2023-05-06 23:45:46 -0300

    bootstrap upgrade

--- bootstrap_tmux.sh
+++ bootstrap_tmux.sh
@@ -4,4 +4,7 @@ cd
 git clone https://github.com/gpakosz/.tmux.git ".config/oh-my-tmux"
 ln -s .config/oh-my-tmux/.tmux.conf .tmux.conf
 
+git clone git@github.com:clyra/dottmux.git ".config/oh-my-tmux-local"
+ln -s .config/oh-my-tmux-local/.tmux.conf.local .tmux.conf.local
+
 
